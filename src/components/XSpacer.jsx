f6deb32cd37d124561239dec7f453c7bede014ee
sangbumGoh <sangbum.goh@weversecompany.com>
2022-12-18 01:17:41 +0900

    build 재수정

--- src/components/XSpacer.jsx
+++ src/components/XSpacer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
-export default styled.div`
+const XSpacer = styled.div`
   flex-grow: 1000;
   height: 100%;
   width: 100%;
 `;
-export default Spacer;
+
+export default XSpacer;
